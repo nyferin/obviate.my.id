4311d53f1ac14577eb4e2d7215603516d6a42e54
nyferin <anthonyfebrian01@gmail.com>
2025-04-05 21:29:00 +0700

    feat: add Interests section

---
+++ src/components/interests.tsx
@@ -0,0 +1,51 @@
+export function Interests() {
+  return (
+    <>
+      <h2 className="text-current/75">
+        <span className="font-semibold">Interests</span>
+      </h2>
+      <p>Let&apos;s see...</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac
+        scelerisque nibh. Pellentesque porttitor, nulla quis aliquam feugiat,
+        nisl libero feugiat urna, a suscipit nunc est nec tortor. Nunc eget
+        nulla non lectus ultricies commodo. Nullam rhoncus augue interdum,
+        luctus turpis vitae, molestie ipsum. Suspendisse feugiat auctor
+        venenatis. Duis mattis felis libero, eu eleifend quam maximus in.
+        Vestibulum mi nisl, auctor vitae maximus sit amet, ullamcorper ut ante.
+        Proin efficitur aliquam metus nec blandit. Praesent justo magna,
+        accumsan eget aliquam eget, elementum at neque. Proin vitae ultricies
+        nisl, eu semper tellus.
+        <br />
+        Quisque condimentum quam sit amet lorem gravida placerat convallis eget
+        purus. In sed odio pretium, ullamcorper sem quis, posuere lacus. Aliquam
+        erat volutpat. Nunc luctus scelerisque justo, id finibus ligula posuere
+        vitae. Donec fermentum eros vel elit auctor, eu rutrum velit porta.
+        Donec sagittis ex at tortor eleifend, non finibus enim maximus. Fusce
+        pellentesque erat ut maximus suscipit. Etiam tristique, ex vitae laoreet
+        interdum, lorem est faucibus massa, vel consequat ipsum nunc in neque.
+        Suspendisse tristique auctor tellus, congue luctus tellus mattis
+        lobortis. Etiam sit amet tellus tempor, scelerisque ex ac, imperdiet
+        ipsum. Cras maximus tempor eros id maximus. Vivamus eget mollis mi.
+        <br />
+        Nulla sodales sem at enim euismod, sed pretium augue tincidunt. Duis sed
+        iaculis dolor. Vestibulum ac bibendum tortor, a imperdiet neque. Sed
+        iaculis tincidunt tincidunt. Integer ut luctus velit. Integer fermentum
+        iaculis justo vitae varius. Donec vestibulum turpis urna, nec tempus ex
+        consectetur non. Aenean ac condimentum elit, eget blandit sem. Vivamus
+        in porttitor nunc. Cras ut felis eu eros auctor iaculis. Donec feugiat,
+        justo placerat viverra vulputate, velit dui efficitur lorem, non
+        accumsan tortor dolor eu lectus. Proin fringilla orci non tristique
+        semper. Fusce ac purus sed elit varius volutpat vitae porttitor justo.
+        <br />
+        Suspendisse metus dolor, consectetur in massa sit amet, tempus gravida
+        libero. Donec felis nibh, aliquam sit amet vehicula quis, finibus vitae
+        nisl. Mauris ipsum lacus, hendrerit et arcu vitae, accumsan sollicitudin
+        mauris. Vivamus fermentum sem a quam scelerisque, molestie accumsan
+        dolor luctus. Maecenas id nulla nec ex interdum fermentum. Ut gravida
+        tincidunt magna, ut eleifend libero commodo eu. Ut purus nibh, sodales
+        id odio eu, cursus eleifend odio.
+      </p>
+    </>
+  );
+}
