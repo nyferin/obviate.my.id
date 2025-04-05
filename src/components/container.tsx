1f94724bb666fd66dc26dce10a4cf2642c93d4da
nyferin <anthonyfebrian01@gmail.com>
2025-04-05 21:28:27 +0700

    fix: add className for children

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,7 +1,17 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
   return (
-    <main className="w-full max-w-3xl mx-auto p-5 flex flex-col gap-5 border">
-      {children}
+    <main className="w-full max-w-3xl mx-auto p-5 flex flex-col gap-5">
+      {React.Children.map(children, (child, index) => (
+        <div className="rounded-sm border px-5 py-3 text-justify" key={index}>
+          {child}
+        </div>
+      ))}
     </main>
   );
 }
