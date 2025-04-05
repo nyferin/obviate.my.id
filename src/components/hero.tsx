04739cea62da1953beb399563550d9955bc2e0c2
nyferin <anthonyfebrian01@gmail.com>
2025-04-05 22:50:10 +0700

    feat: add hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,35 @@
+export function Hero() {
+  return (
+    <div className="rounded-sm px-0.5 py-3 text-justify">
+      <div className="flex justify-between">
+        <h2 className="text-current text-lg">
+          <span className="font-bold">Anthony Febrian Aria Sena</span>
+        </h2>
+
+        <div className="flex items-center justify-center gap-3">
+          <p>
+            [
+            <a href="https://github.com/nyferin" target="blank">
+              GitHub
+            </a>
+            ]
+          </p>
+          <p>
+            [
+            <a href="https://linkedin.com/in/nyferin" target="_blank">
+              LinkedIn
+            </a>
+            ]
+          </p>
+          <p>
+            [
+            <a href="mailto:anthonyfebrianariasena@gmail" target="_blank">
+              Mail
+            </a>
+            ]
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
