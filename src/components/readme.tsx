a6c1d99aae5a4188579b47362081a91e37e84ae9
nyferin <anthonyfebrian01@gmail.com>
2025-04-05 21:29:23 +0700

    feat: add ReadMe section

---
+++ src/components/readme.tsx
@@ -0,0 +1,10 @@
+export function ReadMe() {
+  return (
+    <>
+      <h2 className="text-current/75">
+        <span className="font-semibold">Read Me</span>
+      </h2>
+      <p>Hold up!</p>
+    </>
+  );
+}
