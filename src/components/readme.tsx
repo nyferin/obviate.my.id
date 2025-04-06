6dfbc4ef5f8b755c94bc671dceb5dfb457ad4ed2
nyferin <anthonyfebrian01@gmail.com>
2025-04-06 17:31:53 +0700

    fix: border and content

--- src/components/readme.tsx
+++ src/components/readme.tsx
@@ -1,11 +1,17 @@
 export function ReadMe() {
   return (
-    <div className="rounded-sm border text-justify">
-      <h2 className="text-current/75 border-b px-5 py-3">
+    <div className="rounded-sm border border-current/30 text-justify">
+      <h2 className="text-current/75 border-b border-current/50 px-5 py-3">
         <span className="font-semibold">Read Me</span>
       </h2>
       <div className="px-7 py-5">
-        <p>Hold up!</p>
+        <p>
+          Just dropping in to say hello and welcome to my little corner of the
+          internet. I&apos;m Anthony Febrian Aria Sena, currently based in
+          Salatiga, Indonesia. If you&apos;re here for professional
+          opportunities or collaborations, please feel free to reach out via my
+          working email.
+        </p>
       </div>
     </div>
   );
