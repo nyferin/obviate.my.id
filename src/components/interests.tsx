e58896fb71eb583f631e6a3ef2b478acd529db41
nyferin <anthonyfebrian01@gmail.com>
2025-04-07 08:45:54 +0700

    fix: add content

--- src/components/interests.tsx
+++ src/components/interests.tsx
@@ -1,13 +1,23 @@
 export function Interests() {
+  const interests = [
+    "Cybersecurity",
+    "Programming",
+    "Computer Network",
+    "Web Development",
+    "Cloud Computing",
+  ];
+
   return (
     <div className="rounded-sm border border-current/30 text-justify">
       <h2 className="text-current/75 border-b border-current/50 px-5 py-3">
         <span className="font-semibold">Interests</span>
       </h2>
-      <div className="px-7 py-5">
-        <p>
-          <span>Let&apos;s see...</span>
-        </p>
+      <div className="flex flex-wrap justify-center px-7 py-5">
+        {interests.map((interest, index) => (
+          <p key={index} className="w-1/3 text-center py-2">
+            <span>{interest}</span>
+          </p>
+        ))}
       </div>
     </div>
   );
